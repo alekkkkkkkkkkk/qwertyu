a90327148adef9a0838a751a91634de593059480
alekkkkkkkkkkk <77262043+alekkkkkkkkkkk@users.noreply.github.com>
2021-01-11 11:51:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+VANTA.NET({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x204d2e,
+  backgroundColor: 0x0,
+  points: 13.00,
+  maxDistance: 34.00,
+  spacing: 17.00
+})
+
